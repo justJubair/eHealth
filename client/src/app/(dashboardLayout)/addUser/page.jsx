d0d8c1491de0112b90b0da0357c960b88c24c378
Jubair <jubair.ahmed2838@gmail.com>
2024-03-13 20:17:40 +0600

    Add:responsive sidebar added

---
+++ client/src/app/(dashboardLayout)/addUser/page.jsx
@@ -0,0 +1,7 @@
+const AddUserPage = () => {
+    return(
+        <div>
+             <p> Add User page </p>
+        </div>
+    )}
+export default AddUserPage;
--- client/src/components/Dashboard/Sidebar/Sidebar.jsx
+++ client/src/components/Dashboard/Sidebar/Sidebar.jsx
@@ -1,20 +1,23 @@
+"use client";
 // import icons
 import { GiHealthPotion } from "react-icons/gi";
 import { AiOutlineUsergroupAdd } from "react-icons/ai";
 import { MdWidgets } from "react-icons/md";
 import { MdOutlineLogout } from "react-icons/md";
+import { useState } from "react";
 
 const Sidebar = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
   return (
     <>
       <div className="relative">
-        <div className="absolute z-50 flex flex-col w-24 items-center top-4">
-          <GiHealthPotion className="" size={50} />
+        <div onClick={()=> setSidebarOpen(!sidebarOpen)} className="absolute z-50 flex flex-col w-24 items-center top-4">
+          <GiHealthPotion size={40} />
         </div>
-        <div className=" bg-[#90A1DC] h-screen w-24  pt-4 pb-10">
+        <div className={ `${sidebarOpen ? "-ml-32" : ""} bg-[#90A1DC] h-screen w-24  pt-4 pb-10 duration-200 ease-in-out`}>
           <div className="flex flex-col items-center justify-between h-full">
             {/* Sidebar routes */}
-            <div className="flex flex-col items-center gap-6 mt-[75px]">
+            <div className="flex flex-col items-center gap-6 mt-[65px]">
               <MdWidgets size={30} className="text-white" />
               <AiOutlineUsergroupAdd size={30} className="text-white" />
             </div>
