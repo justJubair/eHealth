6bf35fafefb79a230ea7ad0dc51e29482d983c6c
Jubair <jubair.ahmed2838@gmail.com>
2024-03-14 12:32:23 +0600

    Add: initial server setup

---
+++ server/index.js
@@ -0,0 +1,19 @@
+require("dotenv").config()
+const express = require("express")
+const cors = require("cors")
+const app = express()
+const port = process.env.PORT || 5000
+
+
+// middlewares
+app.use(cors())
+app.use(express.json())
+
+
+app.get("/", (req,res)=>{
+    res.send("eHealth server is working")
+})
+
+app.listen(port, ()=>{
+    console.log(`eHealth server is running on port ${port}`)
+})
